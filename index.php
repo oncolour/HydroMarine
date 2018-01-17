5b7a249f7815f1f631f589c48f46b056d2eb036f
Oscar Nilsson <oscar@oncolour.se>
2018-01-17 17:12:44 +0100

    adding html and css for teasers

--- index.php
+++ index.php
@@ -11,7 +11,37 @@ require 'includes/page_header.php'; ?>
     </section>
     
 <section class="teasers">
-
+    
+    <div class="large">
+        <a href="#">
+            <img src="images/hydrolift-x-22-2018-7.png" alt="x-22 Hydrolift x-22 2018 models available now">
+        </a>
+    </div>
+    
+    <article class="small">
+        <div class="img-bg" style="background-image: url(images/X-22-2017-05-0292.jpg);">
+<!--            <img src="images/X-22-2017-05-0292.jpg" alt="">-->
+        </div>    
+        <header>
+            <h3>Hydrolift X-26 S</h3>
+            <span>Kanonbåt med mycket krut</span>
+        </header>
+        <p>The X—22 is all about making the ordinary exceptional. Featuring smart and flexible design solutions</p>
+    </article>
+    
+    <article class="small">
+        <div class="img-bg" style="background-image:url(images/dsc0991-2-crop-u13519.jpg);">
+<!--            <img src="images/dsc0991-2-crop-u13519.jpg" alt="">-->
+        </div>
+        <header>
+            <h3>Möt oss på båtmässan</h3>
+            <span>27/4-2018</span>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus, urna lobortis eleifend dapibus, dui libero pulvinar ligula, sit amet ultrices ipsum ipsum vitae sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </article>
+   
 </section>
+
+<section class=""
 
 <?php require 'includes/page_footer.php' ?>
