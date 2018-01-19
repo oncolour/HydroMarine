2a53640798bcd3c2b84d1560a8185b1c588b9ce0
Oscar Nilsson <oscar@oncolour.se>
2018-01-19 16:43:38 +0100

    almost finished first page

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,17 @@
 <footer id="page-footer">
-    
-   
+    <section class="container">
+        <ul>
+            <li><a href="#">Hydrolift</a></li>
+            <li><a href="#">Båtförmedling</a></li>
+            <li><a href="#">Om oss</a></li>
+            <li><a href="#">Kontakt</a></li>
+        </ul>
+        <img src="images/logo_neg.svg" alt="Hydro Marine Logotype">
+        <address>
+    <span class="strong">Hydro Marine AB</span><br>
+    Biskopsudden, Djurgården<br>
+        <a href="tel:070-123-44-55">070 123 44 55</a> / <a href="tel:073-123-44-55">073 123 44 55</a> <br>
+        <a href="mailto:info@hydromarine.se">info@hydromarine.se</a>
+    </address>
+    </section>
 </footer>
--- index.php
+++ index.php
@@ -3,35 +3,35 @@
 $your_meta_description = "Vi säljer båtar";
 require 'includes/page_header.php'; ?>
 
-    <section class="front-page-hero">
-      
-        <h1>Sveriges enda återförsäljare av <span class="strong">Hydrolift</span></h1>
-        <a href="hydrolift/" class="btn btn-ghost">Läs mer</a>
-       
-    </section>
-    
+<section class="front-page-hero">
+
+    <h1>Sveriges enda återförsäljare av <span class="strong">Hydrolift</span></h1>
+    <a href="hydrolift/" class="btn btn-ghost">Läs mer</a>
+
+</section>
+
 <section class="teasers container">
-    
+
     <div class="large">
         <a href="#">
             <img src="images/hydrolift-x-22-2018-7.png" alt="x-22 Hydrolift x-22 2018 models available now">
         </a>
     </div>
-    
+
     <article class="small">
         <div class="img-bg" style="background-image: url(images/X-22-2017-05-0292.jpg);">
-<!--            <img src="images/X-22-2017-05-0292.jpg" alt="">-->
-        </div>    
+            <!--            <img src="images/X-22-2017-05-0292.jpg" alt="">-->
+        </div>
         <header>
             <h3>Hydrolift X-26 S</h3>
             <span>Kanonbåt med mycket krut</span>
         </header>
-        <p>The X—22 is all about making the ordinary exceptional. Featuring smart and flexible design solutions</p>
+        <p>The X—22 is all about making the ordinary exceptional. Featuring smart and flexible design solutions.</p>
     </article>
-    
+
     <article class="small">
         <div class="img-bg" style="background-image:url(images/dsc0991-2-crop-u13519.jpg);">
-<!--            <img src="images/dsc0991-2-crop-u13519.jpg" alt="">-->
+            <!--            <img src="images/dsc0991-2-crop-u13519.jpg" alt="">-->
         </div>
         <header>
             <h3>Möt oss på båtmässan</h3>
@@ -39,7 +39,7 @@ require 'includes/page_header.php'; ?>
         </header>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus, urna lobortis eleifend dapibus, dui libero pulvinar ligula..</p>
     </article>
-   
+
 </section>
 
 <a href="#" class="big-link">Se alla båtar från hydrolift</a>
